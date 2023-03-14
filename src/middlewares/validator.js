3e02d26d358ec60a1f0f9353ca6da80d3013e81a
Manuel Rojas <manuerjs70@gmail.com>
2023-03-13 18:04:08 -0600

    feat: Add custom middleware to handle authentication for API rest

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,4 +1,5 @@
 export function validator(request, response, next) {
+    //if the to-do doesnt have title or description which are mandatory params
     if(!request.body.title || !request.body.description) {
         return response.status(400).send({
             message: "Missing Information"
