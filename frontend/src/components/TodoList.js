be43cfb6c72a1b2e4e72237cdcce11e5ee19a82d
Manuel Rojas <manuerjs70@gmail.com>
2023-03-14 15:13:08 -0600

    Move frontend files to frontend directory

---
+++ frontend/src/components/TodoList.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const llamarLista = async () => {
+    const respuesta = await fetch('http://localhost:3000/v1/to-dos');
+    const resJson = await respuesta.json();
+    setTodos(resJson.todos);
+    console.log(todos)
+  };
+
+  useEffect(() => {
+    llamarLista();
+  }, []);
+
+  const addTodo = (todo) => {
+    if (!todo.title || /^\s*$/.test(todo.title)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(...todos);
+  };
+
+  const showDescription = (todoId) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.showDescription = !todo.showDescription;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.title || /^\s*$/.test(newValue.title)) {
+      return;
+    }
+
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  const removeTodo = (id) => {
+    const removedArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isDone = !todo.isDone;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <>
+      <h1>What's the Plan for Today?</h1>
+      <TodoForm
+      onSubmit={addTodo}
+      />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+        showDescription={showDescription}
+      />
+    </>
+  );
+}
+
+export default TodoList;
