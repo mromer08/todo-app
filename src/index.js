d4ab3203f8199d570af1b54d4186869040abe0c4
Manuel Rojas <manuerjs70@gmail.com>
2023-03-13 19:51:20 -0600

    UPDATE all the backend is finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { initDB } from "./db/index.js";
+import { TodosRouter } from "./routes/to-dos.router.js";
 
 const api = express();
 
@@ -9,6 +10,7 @@ const apiPort = process.env["APP_ENV"] || 3000;
 api.use(cors()); //Middleware
 api.use(express.json());
 api.use(express.urlencoded({ extended: false }));
+api.use("/v1", TodosRouter);
 
 api.listen(apiPort, () => {
     console.log(`API RUNNING ON PORT ${apiPort}`);
